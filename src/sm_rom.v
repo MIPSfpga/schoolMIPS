09a9ba15962d82f98322fecbf758ad197d642f24
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-06-29 01:49:43 +0300

    refactoring

--- src/sm_rom.v
+++ src/sm_rom.v
@@ -10,15 +10,8 @@ module sm_rom
     reg [31:0] rom [SIZE - 1:0];
     assign data = rom [addr];
 
-    //rom init
-    reg [7:0] hex [4*SIZE - 1:0];
-    integer i;
-
     initial begin
-        $readmemh ("program.hex", hex);
-        for (i = 0; i < SIZE; i = i + 4)
-            rom [i / 4] = { hex [i + 3], hex [i + 2], 
-                            hex [i + 1], hex [i + 0] };
+        $readmemh ("program.hex", rom);
     end
 
 endmodule
