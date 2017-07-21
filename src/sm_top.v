2178b56453ff0f8082365c88eeb709eed6e594f6
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-07-22 00:45:47 +0300

    sm_clk_divider moved to sm_top

--- src/sm_top.v
+++ src/sm_top.v
@@ -57,3 +57,22 @@ module sm_metafilter
     end
 
 endmodule
+
+//tunable clock devider
+module sm_clk_divider
+#(
+    parameter shift = 16
+)
+(
+    input           clkIn,
+    input           rst_n,
+    input   [ 3:0 ] devide,
+    input           enable,
+    output          clkOut
+);
+    wire [31:0] cntr;
+    wire [31:0] cntrNext = cntr + 1;
+    sm_register_we r_cntr(clkIn, rst_n, enable, cntrNext, cntr);
+
+    assign clkOut = cntr[shift + devide];
+endmodule
