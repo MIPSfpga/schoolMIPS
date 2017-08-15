924442b67fd6856d64364a51fd158706a9f50415
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-08-15 22:27:18 +0300

    small rom refactoring

--- src/sm_rom.v
+++ src/sm_rom.v
@@ -4,11 +4,11 @@ module sm_rom
     parameter SIZE = 64
 )
 (
-    input  [31:0] addr,
-    output [31:0] data
+    input  [31:0] a,
+    output [31:0] rd
 );
     reg [31:0] rom [SIZE - 1:0];
-    assign data = rom [addr];
+    assign rd = rom [a];
 
     initial begin
         $readmemh ("program.hex", rom);
