ec946b6330e14677b319c22294c2432071e77194
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-08-29 21:59:58 +0300

    nexys4 & nexus4_ddr support added

---
+++ board/nexys4_ddr/nexys4_ddr.v
@@ -0,0 +1,97 @@
+
+module nexys4_ddr
+(
+    input         CLK100MHZ,
+    input         CPU_RESETN,
+
+    input         BTNC,
+    input         BTNU,
+    input         BTNL,
+    input         BTNR,
+    input         BTND,
+
+    input  [15:0] SW, 
+
+    output [15:0] LED,
+
+    output        LED16_B,
+    output        LED16_G,
+    output        LED16_R,
+    output        LED17_B,
+    output        LED17_G,
+    output        LED17_R,
+
+    output        CA,
+    output        CB,
+    output        CC,
+    output        CD,
+    output        CE,
+    output        CF,
+    output        CG,
+    output        DP,
+
+    output [ 7:0] AN,
+
+    inout  [12:1] JA,
+    inout  [12:1] JB,
+
+    input         UART_TXD_IN
+);
+
+    // wires & inputs
+    wire          clk;
+    wire          clkIn     =  CLK100MHZ;
+    wire          rst_n     =  CPU_RESETN;
+    wire          clkEnable =  SW [9] | ~BTNU;
+    wire [  3:0 ] clkDevide =  SW [8:5];
+    wire [  4:0 ] regAddr   =  SW [4:0];
+    wire [ 31:0 ] regData;
+
+    //cores
+    sm_top sm_top
+    (
+        .clkIn      ( clkIn     ),
+        .rst_n      ( rst_n     ),
+        .clkDevide  ( clkDevide ),
+        .clkEnable  ( clkEnable ),
+        .clk        ( clk       ),
+        .regAddr    ( regAddr   ),
+        .regData    ( regData   )
+    );
+
+    //outputs
+    assign LED[0]    = clk;
+    assign LED[15:1] = regData[14:0];
+
+    //hex out
+    wire [ 31:0 ] h7segment = regData;
+    wire clkHex;
+
+    sm_clk_divider hex_clk_divider
+    (
+        .clkIn   ( clkIn  ),
+        .rst_n   ( rst_n  ),
+        .devide  ( 4'b1   ),
+        .enable  ( 1'b1   ),
+        .clkOut  ( clkHex )
+    );
+
+    sm_hex_display_8 sm_hex_display_8
+    (
+        .clock          ( clkHex                         ),
+        .resetn         ( rst_n                          ),
+        .number         ( h7segment                      ),
+
+        .seven_segments ( { CG, CF, CE, CD, CC, CB, CA } ),
+        .dot            ( DP                             ),
+        .anodes         ( AN                             )
+    );
+
+    assign LED16_B = 1'b0;
+    assign LED16_G = 1'b0;
+    assign LED16_R = 1'b0;
+    assign LED17_B = 1'b0;
+    assign LED17_G = 1'b0;
+    assign LED17_R = 1'b0;
+
+endmodule
--- src/sm_hex_display.v
+++ src/sm_hex_display.v
@@ -26,3 +26,63 @@ module sm_hex_display
         endcase
 
 endmodule
+
+//--------------------------------------------------------------------
+
+module sm_hex_display_8
+(
+    input             clock,
+    input             resetn,
+    input      [31:0] number,
+
+    output reg [ 6:0] seven_segments,
+    output reg        dot,
+    output reg [ 7:0] anodes
+);
+
+    function [6:0] bcd_to_seg (input [3:0] bcd);
+
+        case (bcd)
+        'h0: bcd_to_seg = 'b1000000;  // a b c d e f g
+        'h1: bcd_to_seg = 'b1111001;
+        'h2: bcd_to_seg = 'b0100100;  //   --a--
+        'h3: bcd_to_seg = 'b0110000;  //  |     |
+        'h4: bcd_to_seg = 'b0011001;  //  f     b
+        'h5: bcd_to_seg = 'b0010010;  //  |     |
+        'h6: bcd_to_seg = 'b0000010;  //   --g--
+        'h7: bcd_to_seg = 'b1111000;  //  |     |
+        'h8: bcd_to_seg = 'b0000000;  //  e     c
+        'h9: bcd_to_seg = 'b0011000;  //  |     |
+        'ha: bcd_to_seg = 'b0001000;  //   --d-- 
+        'hb: bcd_to_seg = 'b0000011;
+        'hc: bcd_to_seg = 'b1000110;
+        'hd: bcd_to_seg = 'b0100001;
+        'he: bcd_to_seg = 'b0000110;
+        'hf: bcd_to_seg = 'b0001110;
+        endcase
+
+    endfunction
+
+    reg [2:0] i;
+
+    always @ (posedge clock or negedge resetn)
+    begin
+        if (! resetn)
+        begin
+            seven_segments <=   bcd_to_seg (0);
+            dot            <= ~ 0;
+            anodes         <= ~ 8'b00000001;
+
+            i <= 0;
+        end
+        else
+        begin
+            seven_segments <=   bcd_to_seg (number [i * 4 +: 4]);
+            dot            <= ~ 0;
+            anodes         <= ~ (1 << i);
+
+            i <= i + 1;
+        end
+    end
+
+endmodule
