a17e204c53d8443d8a6e9fee4ded850adbf9c65c
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-09-10 16:13:16 +0300

    windows script init added

--- program/01_fibonacci/03_generate_verilog_readmemh_file.bat
+++ program/01_fibonacci/03_generate_verilog_readmemh_file.bat
@@ -2,4 +2,4 @@ rem 'objcopy -O verilog' produces the x8 output.
 rem As we need x32 we are getting it from disasm results
 
 echo @00000000 > program.hex
-mips-mti-elf-objdump -D program.elf | ..\bin\sed -rn 's/\s+[a-f0-9]+:\s+([a-f0-9]*)\s+.*/\1/p' >> program.hex
+mips-mti-elf-objdump -D program.elf | ..\..\scripts\bin\sed -rn 's/\s+[a-f0-9]+:\s+([a-f0-9]*)\s+.*/\1/p' >> program.hex
