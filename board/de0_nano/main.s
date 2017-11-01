3b3af5f818f406232e650687e95a3ac651c376c8
Юля <>
2017-11-01 12:03:45 +0300

    DE0-Nano

--- board/de0_nano/main.s
+++ board/de0_nano/main.s
@@ -25,13 +25,12 @@
 
 	cycle1:		addiu	$t0, $t0, 1		 # i = i + 1
 				move	$s3, $s0		 # z = x
+				move 	$v0, $s3		 # print intermediate result
 	
 				li	 	$t1, 0			 # j = 0
 	cycle2:		addiu	$t1, $t1, 1		 # j = j + 1
 				addu	$s0, $s0, $s3 	 # x = x + z
-				move 	$v0, $s3		 # print z
 				bne		$t1, $t2, cycle2 # if j != a-1 goto cycle2 
-				move	$s3, $s0		 # z = x
 				bne		$t0, $t3, cycle1 # if i != m-1 goto cycle1
 				
 				move 	$v0, $s0		 # print x
