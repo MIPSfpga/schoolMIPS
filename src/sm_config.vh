d36cd1d4f16331786a766c72a000fab22fe51cea
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-11-04 22:10:21 +0300

    Simple system bus & gpio added

---
+++ src/sm_config.vh
@@ -0,0 +1,6 @@
+
+// GPIO port width
+//  min value 1
+//  max value 32
+`define SM_GPIO_WIDTH       16
+
--- src/sm_register.v
+++ src/sm_register.v
@@ -1,31 +1,37 @@
 
 
 module sm_register
+#(
+    parameter SIZE = 32
+)
 (
-    input                 clk,
-    input                 rst,
-    input      [ 31 : 0 ] d,
-    output reg [ 31 : 0 ] q
+    input                       clk,
+    input                       rst,
+    input      [ SIZE - 1 : 0 ] d,
+    output reg [ SIZE - 1 : 0 ] q
 );
     always @ (posedge clk or negedge rst)
         if(~rst)
-            q <= 32'b0;
+            q <= { SIZE { 1'b0}};
         else
             q <= d;
 endmodule
 
 
 module sm_register_we
+#(
+    parameter SIZE = 32
+)
 (
-    input                 clk,
-    input                 rst,
-    input                 we,
-    input      [ 31 : 0 ] d,
-    output reg [ 31 : 0 ] q
+    input                       clk,
+    input                       rst,
+    input                       we,
+    input      [ SIZE - 1 : 0 ] d,
+    output reg [ SIZE - 1 : 0 ] q
 );
     always @ (posedge clk or negedge rst)
         if(~rst)
-            q <= 32'b0;
+            q <= { SIZE { 1'b0}};
         else
             if(we) q <= d;
 endmodule
