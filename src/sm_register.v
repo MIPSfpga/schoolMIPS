0fb22187ce385284f2e2f575b1eb9f9d698f532c
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-12-23 08:36:22 +0300

    sync reset registers added

--- src/sm_register.v
+++ src/sm_register.v
@@ -19,14 +19,14 @@ module sm_register_c
 )
 (
     input                        clk,
-    input                        rst,
+    input                        rst_n,
     input      [ WIDTH - 1 : 0 ] d,
     output reg [ WIDTH - 1 : 0 ] q
 );
     localparam RESET = { WIDTH { 1'b0 } };
 
-    always @ (posedge clk /*or negedge rst*/)
-        if(~rst)
+    always @ (posedge clk or negedge rst_n)
+        if(~rst_n)
             q <= RESET;
         else
             q <= d;
@@ -39,16 +39,53 @@ module sm_register_we
 )
 (
     input                        clk,
-    input                        rst,
+    input                        rst_n,
     input                        we,
     input      [ WIDTH - 1 : 0 ] d,
     output reg [ WIDTH - 1 : 0 ] q
 );
     localparam RESET = { WIDTH { 1'b0 } };
     
-    always @ (posedge clk /*or negedge rst*/)
-        if(~rst)
+    always @ (posedge clk or negedge rst_n)
+        if(~rst_n)
             q <= RESET;
         else
             if(we) q <= d;
+endmodule
+
+module sm_register_cs
+#(
+    parameter WIDTH = 1
+)
+(
+    input                        clk,
+    input                        rst_n,
+    input                        clr_n,
+    input      [ WIDTH - 1 : 0 ] d,
+    output     [ WIDTH - 1 : 0 ] q
+);
+    localparam RESET = { WIDTH { 1'b0 } };
+    wire [ WIDTH - 1 : 0 ] dc = ~clr_n ? RESET : d;
+
+    sm_register_c #(WIDTH) r_cs (clk, rst_n, dc, q);
+
+endmodule
+
+module sm_register_wes
+#(
+    parameter WIDTH = 1
+)
+(
+    input                        clk,
+    input                        rst_n,
+    input                        clr_n,
+    input                        we,
+    input      [ WIDTH - 1 : 0 ] d,
+    output     [ WIDTH - 1 : 0 ] q
+);
+    localparam RESET = { WIDTH { 1'b0 } };
+    wire [ WIDTH - 1 : 0 ] dc = ~clr_n ? RESET : d;
+
+    sm_register_we #(WIDTH) r_cs (clk, rst_n, we, dc, q);
+
 endmodule
