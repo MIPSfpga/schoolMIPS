441984bfa07edfaffebefca03a04328b9e29d0d6
Stanislav Zhelnio <zhelniosl@yandex.ru>
2018-01-15 16:53:02 +0300

    pipeline EPC forwarding

--- program/05_exc_ri/modelsim_script.tcl
+++ program/05_exc_ri/modelsim_script.tcl
@@ -20,7 +20,7 @@ vsim -novopt work.sm_testbench
 # add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/rf/*
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/rf/rf
 
-add wave sim:/sm_testbench/cycle 
+add wave -radix unsigned sim:/sm_testbench/cycle 
 add wave -radix hex sim:/sm_testbench/clk 
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_StatusIE 
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_CauseTI 
