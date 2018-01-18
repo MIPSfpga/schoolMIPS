3ad0b714661260055ed420f2131e98ce87ad3b12
Stanislav Zhelnio <zhelniosl@yandex.ru>
2018-01-18 12:06:04 +0300

    irq examples tcl's clean to work on pipeline and simple versions

--- program/05_exc_ri/modelsim_script.tcl
+++ program/05_exc_ri/modelsim_script.tcl
@@ -24,8 +24,6 @@ add wave -radix unsigned sim:/sm_testbench/cycle
 add wave -radix hex sim:/sm_testbench/clk 
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_StatusIE 
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_CauseTI 
-# add wave -height 74 -scale 0.2 -radix hex -format analog-step  sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_Count 
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_Compare 
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_Cause 
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_Status 
 
@@ -33,21 +31,10 @@ add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_control/branch
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_ExcSync 
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_RequestForSync 
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_StatusEXL 
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/cp0_ExcSync
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/cp0_ExcAsync
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/pc 
+
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_EPC 
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_cpz/cp0_ExcHandler 
-
 add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/sm_control/exception 
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/irqRequest_F 
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/irqRequest_D 
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/irqRequest_E 
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/irqRequest_M 
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/epcNext_D 
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/epcNext_E 
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/epcNext_E_new 
-# add wave -radix hex sim:/sm_testbench/sm_top/sm_cpu/epcNext_M 
 
 run -all
 
