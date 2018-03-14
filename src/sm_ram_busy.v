4140c2f067b56e258618ff72004480a95bfe5726
Stanislav Zhelnio <zhelniosl@yandex.ru>
2018-03-14 12:49:48 +0300

    delay module separated

--- src/sm_ram_busy.v
+++ src/sm_ram_busy.v
@@ -3,7 +3,7 @@
 module sm_ram_busy
 #(
     parameter SIZE = 64, // memory size, in words
-    parameter DELAY = 7  // busy delay, from 2 to 255
+    parameter DELAY = 4  // busy delay, from 2 to 255
 )
 (
     input             clk,
@@ -12,13 +12,57 @@ module sm_ram_busy
     input             we,       // write enable
     input      [31:0] wd,       // write data
     input             valid,    // read/write request
-    output reg        ready,    // read/write done
+    output            ready,    // read/write done
     output     [31:0] rd        // read data
 );
+    // Input data buffer
+    wire        ibuf_we;
+    wire [31:0] ram_a;
+    wire [31:0] ram_wd;
+    wire        ram_we;
+    sm_register_we #(32) r_ram_a  (clk, rst_n, ibuf_we, a,  ram_a );
+    sm_register_we #(32) r_ram_wd (clk, rst_n, ibuf_we, wd, ram_wd);
+    sm_register_we       r_ram_we (clk, rst_n, ibuf_we, we, ram_we);
+
+    // Valid to Ready delay 
+    sm_delay #(DELAY) dly 
+    (
+        .clk   ( clk     ),
+        .rst_n ( rst_n   ),
+        .valid ( valid   ),
+        .ready ( ready   ),
+        .start ( ibuf_we )
+    );
+
+    // Requested Memory
+    sm_ram #(SIZE) ram
+    (
+        .clk   ( clk     ),
+        .a     ( ram_a   ),
+        .we    ( ram_we  ),
+        .wd    ( ram_wd  ),
+        .rd    ( rd      )
+    );
+
+endmodule
+
+module sm_delay
+#(
+    parameter DELAY = 2 // busy delay, from 2 to 255
+)
+(
+    input       clk,
+    input       rst_n,
+    input       valid,  // read/write request
+    output reg  ready,  // read/write done
+    output reg  start   // work started strobe
+);
+    localparam EFFECTIVE_DELAY = (DELAY < 2  ) ? 0   :
+                                 (DELAY > 257) ? 255 : DELAY-2;
     // FSM States
     localparam  S_IDLE  = 2'h0, // waiting for request
-                S_BUSY  = 2'h1, // waiting for data, not ready
-                S_READY = 2'h2; // data ready
+                S_BUSY  = 2'h1, // waiting for delay
+                S_READY = 2'h2; // delay finished
 
     // FSM State vars
     wire [1:0] state;
@@ -30,7 +74,7 @@ module sm_ram_busy
     sm_register #(8) r_delay (clk, delay_next, delay);
 
     // Next State value
-    wire delay_finished = (delay == DELAY-2);
+    wire delay_finished = (delay == EFFECTIVE_DELAY);
     assign delay_next = (state == S_BUSY) ? delay + 1 : 0;
 
     always @(*) begin
@@ -45,35 +89,18 @@ module sm_ram_busy
     // FSM Output
     always @(*) begin
         ready = 1'b0;
+        start = 1'b0;
         case(state)
-            S_IDLE  : ready = ~valid;
+            S_IDLE  : begin ready = ~valid; 
+                            start =  valid; end
             S_BUSY  : ready = 1'b0;
             S_READY : ready = 1'b1;
         endcase
     end
 
-    // Input data buffer
-    wire        ibuf_we = (state_next == S_BUSY);
-    wire [31:0] ram_a;
-    wire [31:0] ram_wd;
-    wire        ram_we;
-    sm_register_we #(32) r_ram_a  (clk, rst_n, ibuf_we, a,  ram_a );
-    sm_register_we #(32) r_ram_wd (clk, rst_n, ibuf_we, wd, ram_wd);
-    sm_register_we       r_ram_we (clk, rst_n, ibuf_we, we, ram_we);
-
-    // Requested Memory
-    sm_ram #(SIZE) ram
-    (
-        .clk ( clk    ),
-        .a   ( ram_a  ),
-        .we  ( ram_we ),
-        .wd  ( ram_wd ),
-        .rd  ( rd     )
-    );
-
 endmodule
 
-
+// Memory wout delay
 module sm_ram_fast
 #(
     parameter SIZE = 64,
