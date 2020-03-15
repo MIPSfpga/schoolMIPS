8c588663ef25e4df6e70f9526d951b36c1a6e8a2
Andrey Popov <a.popov99@mail.ru>
2020-03-15 16:15:41 +0300

    Refactored sm_gpio.v

--- src/sm_gpio.v
+++ src/sm_gpio.v
@@ -13,23 +13,24 @@ module sm_gpio
     input  [sm_config::GPIO_WIDTH - 1:0] gpioInput,
     output [sm_config::GPIO_WIDTH - 1:0] gpioOutput
 );
+    localparam BLANK_WIDTH = 32 - sm_config::GPIO_WIDTH;
+
     wire   [sm_config::GPIO_WIDTH - 1:0] gpioIn;    // debounced input signals
     wire                          gpioOutWe; // output Pin value write enable
     wire   [sm_config::GPIO_WIDTH - 1:0] gpioOut;   // output Pin next value
 
-    assign gpioOut   = bWData [sm_config::GPIO_WIDTH - 1:0];
-    assign gpioOutWe = bSel & bWrite & (bAddr[3:0] == sm_config::GPIO_REG_OUTPUT);
-
     sm_debouncer   #(.SIZE(sm_config::GPIO_WIDTH)) debounce(clk, gpioInput, gpioIn);
     sm_register_we #(.SIZE(sm_config::GPIO_WIDTH)) r_output(clk, rst_n, gpioOutWe, gpioOut, gpioOutput);
 
-    localparam BLANK_WIDTH = 32 - sm_config::GPIO_WIDTH;
+    always_comb begin
+        gpioOut   = bWData [sm_config::GPIO_WIDTH - 1:0];
+        gpioOutWe = bSel & bWrite & (bAddr[3:0] == sm_config::GPIO_REG_OUTPUT);
 
-    always @ (*)
         case(bAddr[3:0])
             default              : bRData = { { BLANK_WIDTH {1'b0}}, gpioIn  };
             sm_config::GPIO_REG_INPUT   : bRData = { { BLANK_WIDTH {1'b0}}, gpioIn  };
             sm_config::GPIO_REG_OUTPUT  : bRData = { { BLANK_WIDTH {1'b0}}, gpioOut };
         endcase
+    end
 
-endmodule
+endmodule : sm_gpio
